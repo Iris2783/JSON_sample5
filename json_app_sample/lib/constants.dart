b26810be0ae16af67753af12ed3e0cc72a85010e
Iris2783 <iris.corp.2783@gmail.com>
2022-10-30 01:09:30 +0900

    constants作成

--- json_app_sample/lib/constants.dart
+++ json_app_sample/lib/constants.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-var myBackgroundColor;
+var myBackgroundColor = {};
 
 var myAppBar;
 
 var myDrawerHeader;
 
 var myDrawerList;
+
+var myPostDeatilsPage;
+
+var myUserDetailsPage;
--- json_app_sample/lib/responsive/responsive_layout.dart
+++ json_app_sample/lib/responsive/responsive_layout.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/material.dart';
 
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScaffold;
+  final Widget tabletScaffold;
+  final Widget desktopScaffold;
+
+  ResponsiveLayout({
+    required this.mobileScaffold,
+    required this.tabletScaffold,
+    required this.desktopScaffold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileScaffold;
+        } else if (constraints.maxWidth < 1100) {
+          return tabletScaffold;
+        } else {
+          return desktopScaffold;
+        }
+      },
+    );
+  }
+}
